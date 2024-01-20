677beb017c57f06816f99895b40a988d04a7feb8
dinhphu2k1-gif <dinhphu012001@gmail.com>
2024-01-20 23:37:11 +0700

    update

--- docker/connect_docker_oracle.sh
+++ docker/connect_docker_oracle.sh
@@ -1,1 +1,3 @@
 docker exec -it oracle sqlplus sys/12012001@alo as sysdba
+
+docker exec -it oracle sqlplus pdbadmin/12012001@dinhphu
--- docker/oracle.sh
+++ docker/oracle.sh
@@ -1,6 +1,7 @@
 docker run -d --name oracle \
  -p 1521:1521 -p 5500:5500 \
  -e ORACLE_SID=alo \
+ -e ORACLE_PDB=dinhphu \
  -e ORACLE_PWD=12012001 \
  -e ENABLE_ARCHIVELOG=true \
-container-registry.oracle.com/database/enterprise
+container-registry.oracle.com/database/enterprise:21.3.0.0
